08b99ae526bbf2b79f8cc1cad56e3b46e7f4d2e9
Michael Kohler <me@michaelkohler.info>
2019-10-08 19:16:51 +0200

    Update dependency

--- packages/eslint-config-vue/index.js
+++ packages/eslint-config-vue/index.js
@@ -5,5 +5,9 @@ module.exports = {
     "vue/html-indent": ["error", 4],
     "filenames/match-regex": [2, "^[A-Za-z0-9-]+$"],
   },
-  extends: ["@michaelkohler/eslint-config-browser", "@michaelkohler/eslint-config-webpack", "plugin:vue/strongly-recommended"],
+  extends: [
+    "@michaelkohler/eslint-config-browser",
+    "@michaelkohler/eslint-config-webpack",
+    "plugin:vue/strongly-recommended",
+  ],
 };
